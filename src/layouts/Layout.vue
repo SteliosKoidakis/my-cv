05c6cef1e2886a5cb49750da53bcb8fff37f0e10
Stylianos Koidakis <Stylianos Koidakis>
2018-11-23 21:13:56 +0100

    Vue and vue router set up

---
+++ src/layouts/Layout.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="Layout">
+    <nav class="Layout__nav">
+      <router-link to="/">Home</router-link> 
+      <router-link to="/about">About</router-link>
+    </nav>
+    <main class="Layout__main">
+      <slot/>
+    </main>
+    <footer class="Layout__footer">
+      &copy; Footer
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {
+  name: `Layout`,
+};
+</script>
+
