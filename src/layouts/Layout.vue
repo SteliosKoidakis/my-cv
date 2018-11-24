fa5fcb10bc2afe180e4706e3803ee4c4359040b7
Stylianos Koidakis <Stylianos Koidakis>
2018-11-24 20:30:14 +0100

    Set media queries and responsive navigation

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="Layout">
-    <nav class="Layout__nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/experience">Experience</router-link>
-      <router-link to="/projects">Projects</router-link>
-      <router-link to="/skills">Skills</router-link>
-      <router-link to="/education">Education</router-link>
+    <nav class="Layout__nav padding-m">
+      <a class="padding-s text-center" v-if="isSmallScreen" @click="triggerMobileMenu">
+        <font-awesome-icon :icon="getArrow" />
+      </a>
+      <template v-if="showMenu">
+        <router-link class="padding-s" to="/">Home</router-link>
+        <router-link class="padding-s" to="/experience">Experience</router-link>
+        <router-link class="padding-s" to="/projects">Projects</router-link>
+        <router-link class="padding-s" to="/skills">Skills</router-link>
+        <router-link class="padding-s" to="/education">Education</router-link>
+      </template>
+
     </nav>
     <main class="Layout__main container">
       <slot/>
     </main>
-    <footer class="Layout__footer">
+    <footer class="Layout__footer padding-l">
       &copy; Footer
     </footer>
   </div>
@@ -19,13 +25,47 @@
 <script>
 export default {
   name: 'Layout',
+  data() {
+    return {
+      mobileMenuStatus: false,
+    };
+  },
+  computed: {
+    isSmallScreen() {
+      return this.$mq === 'mobile' || this.$mq === 'tablet';
+    },
+    showMenu() {
+      return !this.isSmallScreen || this.mobileMenuStatus;
+    },
+    getArrow() {
+      return this.showMenu ? 'angle-double-up' : 'angle-double-down';
+    },
+  },
+  methods: {
+    triggerMobileMenu() {
+      this.mobileMenuStatus = !this.mobileMenuStatus;
+    },
+  },
 };
 </script>
 
 <style lang="scss" scoped>
 .Layout {
-  &_nav {
-    padding: 20px;
+  &__nav {
+    background-color: black;
+    a {
+      font-weight: 700;
+      color: white;
+      @media (max-width:680px) {
+        display: block;
+      }
+    }
+    a:hover {
+      text-decoration: none;
+      color: white;
+      cursor: pointer;
+      background-color: darkslategray;
+    }
   }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,8 +3,25 @@ import BootstrapVue from 'bootstrap-vue';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-vue/dist/bootstrap-vue.css';
 import './styles/index.scss';
-import App from './App.vue';
+import VueMq from 'vue-mq';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faAngleDoubleDown, faAngleDoubleUp } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import router from './router';
+import App from './App.vue';
+
+library.add(faAngleDoubleDown, faAngleDoubleUp);
+Vue.config.productionTip = false;
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+
+Vue.use(VueMq, {
+  breakpoints: {
+    mobile: 450,
+    tablet: 680,
+    desktop: Infinity,
+  },
+});
 
 Vue.use(BootstrapVue);
 
